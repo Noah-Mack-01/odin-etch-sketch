c642a4ae824d7696ad382ce2f3863b268be991f5
Noah-Mack-01 <noahnewmanmack@gmail.com>
2022-11-15 15:03:00 -0500

    Add button listener for resolution change
    Resolution is now changeable given user button.

--- tile-add.js
+++ tile-add.js
@@ -3,12 +3,26 @@
 
 let gridSet = (num) => {
     if (num > 100) {console.log("Error! Cannot process that big of a grid."); return;}
+
+    let style =document.createAttribute
+    document.getElementById("tile-container").innerHTML="";
+
     for (i = 0; i < num*num; i++) {
         let tile = document.createElement("div");
         tile.classList.add("tile");
+        tile.style.width = `${80/num}vh`;
+        tile.style.height= `${80/num}vh`;
         document.getElementById("tile-container").appendChild(tile);
         console.log("added child.");
     }
 }
 
-gridSet(16);
+
+document.getElementById("onButton").addEventListener("click", () => {
+    let res = prompt("What should be resolution of grid?");
+    gridSet(+res);
+});
+
+gridSet(20);
+
+
