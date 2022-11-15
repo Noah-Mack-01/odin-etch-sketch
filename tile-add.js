27af1a02a28782c5b3ec7a66c2b858d1e18271cc
Noah-Mack-01 <noahnewmanmack@gmail.com>
2022-11-15 13:30:17 -0500

    Remove fixed tile size, turn script into function.
    Turned the script of tile-add.js into a single function
    within itself, labeled gridSet(num). Added validation
    to the function to ensure that the number is < 100.
    
    Need to add functionality to overwrite the existing container
    with javascript upon input of a new number, and need to
    write out the input form in html.

--- tile-add.js
+++ tile-add.js
@@ -1,8 +1,14 @@
 
-for (i = 0; i < 256; i++) {
-    let tile = document.createElement("div");
-    tile.classList.add("tile");
-    document.getElementById("tile-container").appendChild(tile);
-    console.log("added child.");
+
+
+let gridSet = (num) => {
+    if (num > 100) {console.log("Error! Cannot process that big of a grid."); return;}
+    for (i = 0; i < num*num; i++) {
+        let tile = document.createElement("div");
+        tile.classList.add("tile");
+        document.getElementById("tile-container").appendChild(tile);
+        console.log("added child.");
+    }
 }
 
+gridSet(16);
